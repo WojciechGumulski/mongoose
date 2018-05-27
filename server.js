42608e997981f42c3386bec1d10451757bed0858
Wojtek Gumulski <wojciech.gumulski@gmail.com>
2018-05-27 21:58:08 +0200

    Add user

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb:/User1:12345@ds237610.mlab.com:37610/mongoose', {
     useMongoClient: true
 });
 
